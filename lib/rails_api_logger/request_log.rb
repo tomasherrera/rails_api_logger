a8c099f1b873ca7d2b9202b3be6aed8d09d63c15
Tomas Herrera <tomasherrera91@icloud.com>
2022-08-05 10:48:14 -0500

    Add headers to requests logs

--- lib/rails_api_logger/request_log.rb
+++ lib/rails_api_logger/request_log.rb
@@ -14,12 +14,13 @@ class RequestLog < ActiveRecord::Base
   def self.from_request(request, loggable: nil)
     request_body = (request.body.respond_to?(:read) ? request.body.read : request.body)
     body = request_body ? request_body.dup.force_encoding("UTF-8") : nil
+    headers = request&.each_header&.to_h&.to_json || {}
     begin
       body = JSON.parse(body) if body.present?
     rescue JSON::ParserError
       body
     end
-    create(path: request.path, request_body: body, method: request.method, started_at: Time.current, loggable: loggable)
+    create(path: request.path, request_body: body, method: request.method, request_headers: headers, started_at: Time.current, loggable: loggable)
   end
 
   def from_response(response)
